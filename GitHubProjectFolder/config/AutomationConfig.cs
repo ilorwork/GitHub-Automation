bfe25159957d52b2e38317dd4ff77daa39351c31
eilorsh-tr <eilorsh@etoro.com>
2021-07-07 23:46:45 +0300

    some instructions

--- GitHubProjectFolder/config/AutomationConfig.cs
+++ GitHubProjectFolder/config/AutomationConfig.cs
@@ -8,7 +8,12 @@ namespace GitHub.config
 {
     public class AutomationConfig
     {
+        /*
+         * public const int DefaultTimeout = 30;
+         * public const int DefaultTimeoutForLongWaits = 60;
+         */
         public static IWebDriver Driver = new ChromeDriver();
+        // remove this static
         public static WebDriverWait Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
 
         public void Log(string info)
