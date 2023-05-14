475110706cc931565a451770fc6f4344840d2461
ilorwork <ilorwork64@gmail.com>
2023-05-14 17:49:27 +0300

    Add Driver Manager to automaticlly update chrome driver

--- GitHubProjectFolder/config/AutomationConfig.cs
+++ GitHubProjectFolder/config/AutomationConfig.cs
@@ -2,13 +2,20 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace GitHub.config
 {
     public class AutomationConfig
     {
+        public AutomationConfig()
+        {
+            Driver = GetDriver();
+        }
+
         // TODO: add FireFox
-        public static IWebDriver Driver = new ChromeDriver();
+        public static IWebDriver Driver;
         // TODO: add selenium grid
         public bool IsLocal => GetParams("local", true);
         public const int Retries = 1;
@@ -16,5 +23,15 @@ namespace GitHub.config
         public void Log(string info) => LocalLogger.PrintLog(info);
 
         public T GetParams<T>(string param, T defaultV) => TestContext.Parameters.Get(param, defaultV);
+
+        public IWebDriver GetDriver()
+        {
+            if (Driver == null)
+            {
+                new DriverManager().SetUpDriver(new ChromeConfig());
+                return new ChromeDriver();
+            }
+            return Driver;
+        }
     }
 }
